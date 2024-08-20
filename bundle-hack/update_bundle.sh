506d8885c7e7bd5ea48e241895aced1ff0deb3ea
John Kyros <jkyros@redhat.com>
2024-08-19 19:12:30 -0500

    konflux: add bundle dockerfile and hack script

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -1,14 +1,38 @@
 #!/usr/bin/env bash
 
-export GATEKEEPER_GATEKEEPER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256"
+# TODO(jkyros): Fish this out of the operator repo, maybe "most recent manifest version in keda/ or something" 
+# TODO(jkyros): Somehow we need to add that to the FBC also and populate our CI_SPEC_RELEASE
+export VERSION=$(ls custom-metrics-autoscaler-operator/keda/ | sort -rV | head -n1)
+mv /custom-metrics-autoscaler-operator/keda/${VERSION}/manifests/ /manifests/
+mv /custom-metrics-autoscaler-operator/keda/${VERSION}/metadata/ /metadata/
 
-export GATEKEEPER_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper-operator@sha256:aa395edded8dde4a38e1033fa47e2fad6fa0bebc1aa0889d5da47afcb8f34267"
 
-export CSV_FILE=/manifests/gatekeeper-operator.clusterserviceversion.yaml
+export CI_SPEC_RELEASE="454"
 
-sed -i -e "s|quay.io/gatekeeper/gatekeeper:v.*|\"${GATEKEEPER_GATEKEEPER_IMAGE_PULLSPEC}\"|g" \
-	-e "s|quay.io/gatekeeper/gatekeeper-operator:v.*|\"${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC}\"|g" \
-	"${CSV_FILE}"
+# TODO(jkyros): So here's how this works -- we put these variables in here, and then Konflux comes through and updates them when the builds get updated, ideally
+# not causing cyclical builds, so we have to exclude this somehow I think 
+export CMA_OPERATOR_PULLSPEC=quay.io/repository/redhat-user-workloads/cma-podauto-tenant/custom-metrics-autoscaler-operator@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256
+export KEDA_OPERATOR_PULLSPEC=quay.io/repository/redhat-user-workloads/cma-podauto-tenant/keda-operator@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256
+export KEDA_ADAPTER_PULLSPEC=quay.io/repository/redhat-user-workloads/cma-podauto-tenant/keda-adapter@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256
+export KEDA_WEBHOOK_PULLSPEC=quay.io/repository/redhat-user-workloads/cma-podauto-tenant/keda-webhooks@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256
+
+# Since we moved the versioned manifest to /manifests, we can just use it from there
+export CSV_FILE=/manifests/cma.v${VERSION}.clusterserviceversion.yaml
+
+# Update image references to use brew-built images
+# Put the allowed repositories: OSBS proxy and registry.redhat.io.
+# The pinning to SHA1 will happen after thanks to the config in container.yaml.
+# TODO(jkyros): I think the FBC will take care of the replaces, etc based on the catalog so we don't have to do it 
+sed -i -e "s#ghcr.io/kedacore/keda-olm-operator:\(main\|[0-9.]*\)#${CMA_OPERATOR_PULLSPEC}#g" \
+       -e "s#CMA_OPERAND_PLACEHOLDER_1\$#${KEDA_OPERATOR_PULLSPEC}#g" \
+       -e "s#CMA_OPERAND_PLACEHOLDER_2\$#${KEDA_ADAPTER_PULLSPEC}#g" \
+       -e "s#CMA_OPERAND_PLACEHOLDER_3\$#${KEDA_WEBHOOK_PULLSPEC}#g" \
+       -e "s/^\(  *\)olm.skipRange: '>=2.7.1 <[0-9.-]*'/\1olm.skipRange: '>=2.7.1 <${VERSION}-${CI_SPEC_RELEASE}'/" \
+       -e '/^metadata:$/,/^spec:$/ s/^\(  name: custom-metrics-autoscaler\.v\)[0-9.]*$/\1'"${VERSION}-${CI_SPEC_RELEASE}"'/' \
+       -e '/^spec:$/,$ s/^\(  version: \)[0-9.]*$/\1'"${VERSION}-${CI_SPEC_RELEASE}"'/' \
+       "${CSV_FILE}"
+cat "${CSV_FILE}"
+
 
 export AMD64_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="amd64")')
 export ARM64_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="arm64")')
@@ -60,10 +84,11 @@ gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/tls-p
 gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-aws'] = 'false'
 gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-azure'] = 'false'
 gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-gcp'] = 'false'
-gatekeeper_csv['metadata']['annotations']['repository'] = 'https://github.com/stolostron/gatekeeper-operator'
-gatekeeper_csv['metadata']['annotations']['containerImage'] = os.getenv('GATEKEEPER_OPERATOR_IMAGE_PULLSPEC', '')
+gatekeeper_csv['metadata']['annotations']['repository'] = 'https://github.com/openshift/custom-metrics-autoscaler-operator'
+gatekeeper_csv['metadata']['annotations']['containerImage'] = os.getenv('CMA_OPERATOR_PULLSPEC', '')
 
 dump_manifest(os.getenv('CSV_FILE'), gatekeeper_csv)
 CSV_UPDATE
 
 cat $CSV_FILE
+
