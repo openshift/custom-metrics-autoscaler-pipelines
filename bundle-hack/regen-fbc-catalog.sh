47747ae3adb82166a740417a49951e7c6229da03
John Kyros <jkyros@redhat.com>
2024-10-22 17:13:54 -0500

    konflux: fix fbc for staging
    Previously we had a version on our fbc, but we're rolling release so I
    don't know that it matters (aside from our containerfile opm version
    matching the openshift release, which I think we still need to be
    careful of)

--- bundle-hack/regen-fbc-catalog.sh
+++ bundle-hack/regen-fbc-catalog.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-# TODO(jkyros): I used a template to generate the actual catalog, this script needs to be better/different but 
-# opm interrogates all the bundles in the template, snarfs out their details, and then renders a final catalog based on 
-# those details  
+# TODO(jkyros): I used a template to generate the actual catalog, this script needs to be better/different but
+# opm interrogates all the bundles in the template, snarfs out their details, and then renders a final catalog based on
+# those details
 # TODO(jkyros): What is the downside if I do this generation inside the catalog dockerfile and never commit it back?  It would save a manual step. i'd
 # really like to be able to just have something automatically regen the catalog.yaml and PR it back with renovate or something
 for CATALOG_VERSION in catalogs/*; do
-opm alpha render-template basic ${CATALOG_VERSION}/catalog-template.yaml -o yaml > ${CATALOG_VERSION}/catalog/custom-metrics-autoscaler-operator/catalog.yaml
+CATALOG_OUTPUT=${CATALOG_VERSION}/catalog/custom-metrics-autoscaler-operator/catalog.yaml
+# This is basically "generate the catalog, and then rewrite the pullspecs to match their final resting place". You can't do it beforehand in the
+# template because render-template pulls all those images to check them to include them in the catalog (and the new ones won't be there yet because they
+# haven't shipped)
+opm alpha render-template basic ${CATALOG_VERSION}/catalog-template.yaml -o yaml > $CATALOG_OUTPUT
+    #  --- Comet Mangling ---
+    # Version of RHEL we ship on top of, corresponds to comet repository names which have os version suffixes.
+    # We have to do this here because the pipeline is signed and sealed, so we can't change anything after it's built.
+    OSVER="rhel9"
+    APP_PREFIX="quay.io/redhat-user-workloads/cma-podauto-tenant/custom-metrics-autoscaler-operator"
+    PROD_PREFIX="registry.redhat.io/custom-metrics-autoscaler"
+    sed -i "s|$APP_PREFIX/keda-adapter|$PROD_PREFIX/keda-adapter-$OSVER|g" $CATALOG_OUTPUT
+    sed -i "s|$APP_PREFIX/keda-operator|$PROD_PREFIX/keda-operator-$OSVER|g" $CATALOG_OUTPUT
+    sed -i "s|$APP_PREFIX/keda-webhook|$PROD_PREFIX/keda-webhook-$OSVER|g" $CATALOG_OUTPUT
+    sed -i "s|$APP_PREFIX/custom-metrics-autoscaler-operator-bundle|$PROD_PREFIX/custom-metrics-autoscaler-konflux-operator-bundle|g" $CATALOG_OUTPUT
+    sed -i "s|$APP_PREFIX/custom-metrics-autoscaler-operator|$PROD_PREFIX/custom-metrics-autoscaler-operator-$OSVER|g" $CATALOG_OUTPUT
+
+
+    # This is for all the old brew builds if we keep them in our catalog:
+    sed -i 's|brew.registry.redhat.io/custom-metrics-autoscaler/custom-metrics-autoscaler|registry.redhat.io/custom-metrics-autoscaler/custom-metrics-autoscaler|g' $CATALOG_OUTPUT
 done
