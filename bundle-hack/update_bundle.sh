fa97cdb88f88c4f5e8f829b6ee2fe36b220c19d3
John Kyros <jkyros@redhat.com>
2026-05-29 20:02:34 -0500

    Fix sed expression for http operand replacement
    The sed expression wasn't working right for the replace, this fixes it.
    This will come out eventually once the csv itself gets fixed.
    
    Signed-off-by: John Kyros <jkyros@redhat.com>

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -101,12 +101,20 @@ sed -i -e "s#ghcr.io/kedacore/keda-olm-operator:\(main\|[0-9.]*\)#${CMA_OPERATOR
        -e "s#CMA_OPERAND_PLACEHOLDER_5\$#${HTTP_ADDON_OPERATOR_PULLSPEC}#g" \
        -e "s#CMA_OPERAND_PLACEHOLDER_6\$#${HTTP_ADDON_SCALER_PULLSPEC}#g" \
        -e "s#KEDA_ADMISSION_WEBHOOKS_IMAGE=\\\$RELATED_IMAGE_3; exec /manager#KEDA_ADMISSION_WEBHOOKS_IMAGE=\$RELATED_IMAGE_3; export KEDA_HTTP_ADDON_INTERCEPTOR_IMAGE=\$RELATED_IMAGE_4; export KEDA_HTTP_ADDON_OPERATOR_IMAGE=\$RELATED_IMAGE_5; export KEDA_HTTP_ADDON_SCALER_IMAGE=\$RELATED_IMAGE_6; exec /manager#" \
-       -e "/value: CMA_OPERAND_PLACEHOLDER_3/a\\
-                - name: RELATED_IMAGE_4\n                  value: CMA_OPERAND_PLACEHOLDER_4\n                - name: RELATED_IMAGE_5\n                  value: CMA_OPERAND_PLACEHOLDER_5\n                - name: RELATED_IMAGE_6\n                  value: CMA_OPERAND_PLACEHOLDER_6" \
        -e "s/^\(  *\)olm.skipRange: '>=2.7.1 <[0-9.-]*'/\1olm.skipRange: '>=2.7.1 <${VERSION}-${CI_SPEC_RELEASE}'/" \
        -e '/^metadata:$/,/^spec:$/ s/^\(  name: custom-metrics-autoscaler\.v\)[0-9.-]*$/\1'"${VERSION}-${CI_SPEC_RELEASE}"'/' \
        -e '/^spec:$/,$ s/^\(  version: \)[0-9.-]*$/\1'"${VERSION}-${CI_SPEC_RELEASE}"'/' \
        "${CSV_FILE}"
+
+# TODO(jkyros): clean this up when we fix the CSV bundle and don't need to inject
+# Inject RELATED_IMAGE_4/5/6 env vars after RELATED_IMAGE_3 in a separate pass so the
+# values are already resolved (the old single-pass append could never work because sed
+# replaces the anchor pattern before the append runs, and appended text doesn't re-run
+# earlier substitutions)
+sed -i "/name: RELATED_IMAGE_3/,/value:/{
+  /value:/a\\
+                - name: RELATED_IMAGE_4\n                  value: ${HTTP_ADDON_INTERCEPTOR_PULLSPEC}\n                - name: RELATED_IMAGE_5\n                  value: ${HTTP_ADDON_OPERATOR_PULLSPEC}\n                - name: RELATED_IMAGE_6\n                  value: ${HTTP_ADDON_SCALER_PULLSPEC}
+}" "${CSV_FILE}"
 cat "${CSV_FILE}"
 
 export EPOC_TIMESTAMP=$(date +%s)
